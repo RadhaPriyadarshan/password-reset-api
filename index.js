114cac483b5cea5638a6ffc76dc2a8a3c8a017b2
Radha priyadarsan <radhapriyan786@gmail.com>
2024-06-12 15:06:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://radhapriyan786:radha@forget-password-api.zpuhekg.mongodb.net/users?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
